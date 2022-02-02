2d881bf268547308b6d3f94a0a7aa14c77e3987c
Your Name <you@example.com>
2022-02-02 21:20:56 +0530

    changed

--- fabi/urls.py
+++ fabi/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('fibo_post/', views.fibo_post, name='fibo_post_name'),
-    # path('fibo_archive/', views.fibo_archive, name='fibo_archive'),
+    path('fibo_post/', views.fibo_post, name='fibo_post'),
+    path('fibo_archive/', views.fibo_archive, name='fibo_archive'),
 ]
--- fabi/views.py
+++ fabi/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from fabi.models import Fabonacci
@@ -9,29 +9,43 @@ def index(request):
 
 
 def fibo_post(request):
-    value = request.GET['value']
-    print(value)
+    if request.method == 'GET':
+        value = request.GET['value']
 
-    n1, n2 = 0, 1
-    count = 0
-    series = []
-    # check if the number of terms is valid
-    if int(value) <= 0:
-        print("Please enter a positive integer")
-    # if there is only one term, return n1
-    elif int(value) == 1:
-        series.append(str(n1))
-    # generate fibonacci sequence
-    else:
-        while count < int(value):
+        n1, n2 = 0, 1
+        count = 0
+        series = []
+        # check if the number of terms is valid
+        if int(value) <= 0:
+            print("Please enter a positive integer")
+        # if there is only one term, return n1
+        elif int(value) == 1:
             series.append(str(n1))
-            nth = n1 + n2
-            # update values
-            n1 = n2
-            n2 = nth
-            count += 1
-    p = ' '.join(series)
-    pp = Fabonacci(numstr=value, terms=p)
-    pp.save()
+        # generate fibonacci sequence
+        else:
+            while count < int(value):
+                series.append(str(n1))
+                nth = n1 + n2
+                # update values
+                n1 = n2
+                n2 = nth
+                count += 1
+        p = ' '.join(series)
+        pp = Fabonacci(numstr=value, terms=p)
+        pp.save()
+        context = {
+            'seriess': p
+        }
 
-    return render(request, 'fabi/index.html', {'series': pp})
+        return render(request, 'fabi/index.html', context)
+    else:
+        return redirect(index)
+
+
+def fibo_archive(request):
+    all_series = Fabonacci.objects.all()
+
+    context = {
+        'all_series': all_series
+    }
+    return render(request, 'fabi/index.html', context)
